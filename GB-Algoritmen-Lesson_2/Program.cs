d3920042bc60e3af94d409726de7de2df2dc9667
Arhio <Arhio@yandex.ru>
2019-11-10 07:06:49 +0300

    Алгоритмы. Задание 2. Упражнение 1 и 2. Готовы!

--- GB-Algoritmen-Lesson_2/Program.cs
+++ GB-Algoritmen-Lesson_2/Program.cs
@@ -13,8 +13,9 @@ namespace GB_Algoritmen_Lesson_2
     {
         static Dictionary<string, Act> dict = new Dictionary<string, Act>
         {
-            { "1", new Act },
-        };  
+            { "1", new TranslationToBinary() },
+            { "2", new Exponentiation() },
+        };
 
         static void Main(string[] args)
         {
@@ -26,20 +27,20 @@ namespace GB_Algoritmen_Lesson_2
             while (n != "0")
             {
                 WriteLine("Введите номер интересующей вас задачи:" + Environment.NewLine +
-                    "1.	Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию." + Environment.NewLine +
-                    "2.Реализовать функцию возведения числа a в степень b: " + Environment.NewLine +
-                    "a.Без рекурсии." + Environment.NewLine +
-                    "b.Рекурсивно." + Environment.NewLine +
-                    "c.  * Рекурсивно, используя свойство чётности степени." + Environment.NewLine +
-                    "3.  * *Исполнитель «Калькулятор» преобразует целое число, записанное на экране.У исполнителя две команды, каждой присвоен номер: " + Environment.NewLine +
+                    "1.-Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию." + Environment.NewLine +
+                    "  -Реализовать функцию возведения числа a в степень b: " + Environment.NewLine +
+                    "2.   a.Без рекурсии." + Environment.NewLine +
+                    "3.   b.Рекурсивно." + Environment.NewLine +
+                    "4.   c.*Рекурсивно, используя свойство чётности степени." + Environment.NewLine +
+                    "  -**Исполнитель «Калькулятор» преобразует целое число, записанное на экране.У исполнителя две команды, каждой присвоен номер: " + Environment.NewLine +
                     "      1.Прибавь 1." + Environment.NewLine +
                     "      2.Умножь на 2." + Environment.NewLine +
-                    "   Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза.Определить, сколько существует программ, которые преобразуют число 3 в число 20:" + Environment.NewLine +
-                    "      а.С использованием массива." + Environment.NewLine +
-                    "      b. * С использованием рекурсии." + Environment.NewLine +
+                    "    Первая к оманда увеличивает число на экране на 1, вторая увеличивает его в 2 раза. Определить, сколько существует программ, которые преобразуют число 3 в число 20:" + Environment.NewLine +
+                    "5.    а.С использованием массива." + Environment.NewLine +
+                    "6.    b. * С использованием рекурсии." + Environment.NewLine +
                     "0. Нажмите для выхода из программы.");
 
-                n = q.Question<int>("Введите ", new HashSet<char>() { '0', '1', '2', '3', '4' }, true);
+                n = q.Question<int>("Ваша команда ", new HashSet<char>() { '0', '1', '2', '3', '4' }, true);
                 if (n == "0") break;
                 dict[n].Work();
             }
@@ -51,5 +52,60 @@ namespace GB_Algoritmen_Lesson_2
     abstract class Act
     {
         public abstract void Work();
+    }
+
+    class TranslationToBinary : Act
+    {
+        public override void Work() => WriteLine($"Двоичным представлением числа будет: { ToBinary(int.Parse((new Questions()).Question<int>("Введите число:", new HashSet<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, true))) }");
+        
+        public string ToBinary(double num) => Convert(new StringBuilder(), (int)Math.Sqrt(num), ref num).ToString();
+        
+        private StringBuilder Convert(StringBuilder txt, int step, ref double number)
+        {
+            if (number <= 0 && step <= 0) return txt;
+            var t = step * step;
+            if (t == 0) t = 1;
+            var answer = "0";
+            if (t <= number)
+            {
+                answer = "1";
+                number -= t;
+            }
+            step--;
+            txt.Append(answer);
+            Convert(txt, step, ref number);
+            return txt;
+        }
+    }
+
+    class Exponentiation : Act
+    {
+        public override void Work()
+        {
+            var number = int.Parse((new Questions()).Question<int>("Введите число:", new HashSet<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, true));
+            var degree = int.Parse((new Questions()).Question<int>("Введите степень:", new HashSet<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, true));
+            WriteLine($" {ExponentNotRecuresion(number, degree)}");
+        }
+
+        private string ExponentNotRecuresion(int number, int degree)
+        {
+            var n = number;
+            var d = 1;
+            while(true)
+            {
+                d = d + 2;
+                if (degree < d) break;
+                number = number * number;
+                degree -= d;
+            }             
+         
+            while(degree > 0)
+            {
+                number = number * n;
+                degree--;
+            }
+
+            return number.ToString();
+        }
     }
 }
