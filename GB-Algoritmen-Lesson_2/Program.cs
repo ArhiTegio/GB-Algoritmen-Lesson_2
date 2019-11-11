ac8e623176de98da04fb6d6bc65c008abe7601ae
Arhio <Arhio@yandex.ru>
2019-11-11 20:48:42 +0300

    Готово кроме 3.а

--- GB-Algoritmen-Lesson_2/Program.cs
+++ GB-Algoritmen-Lesson_2/Program.cs
@@ -15,6 +15,7 @@ namespace GB_Algoritmen_Lesson_2
         {
             { "1", new TranslationToBinary() },
             { "2", new Exponentiation() },
+            { "3", new Calculator() },
         };
 
         static void Main(string[] args)
@@ -35,9 +36,9 @@ namespace GB_Algoritmen_Lesson_2
                     "  -**Исполнитель «Калькулятор» преобразует целое число, записанное на экране.У исполнителя две команды, каждой присвоен номер: " + Environment.NewLine +
                     "      1.Прибавь 1." + Environment.NewLine +
                     "      2.Умножь на 2." + Environment.NewLine +
-                    "    Первая к оманда увеличивает число на экране на 1, вторая увеличивает его в 2 раза. Определить, сколько существует программ, которые преобразуют число 3 в число 20:" + Environment.NewLine +
-                    "5.    а.С использованием массива." + Environment.NewLine +
-                    "6.    b. * С использованием рекурсии." + Environment.NewLine +
+                    "3.-Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза. Определить, сколько существует программ, которые преобразуют число 3 в число 20:" + Environment.NewLine +
+                    "      а.С использованием массива." + Environment.NewLine +
+                    "      b.*С использованием рекурсии." + Environment.NewLine +
                     "0. Нажмите для выхода из программы.");
 
                 n = q.Question<int>("Ваша команда ", new HashSet<char>() { '0', '1', '2', '3', '4' }, true);
@@ -84,8 +85,9 @@ namespace GB_Algoritmen_Lesson_2
         {
             var number = int.Parse((new Questions()).Question<int>("Введите число:", new HashSet<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, true));
             var degree = int.Parse((new Questions()).Question<int>("Введите степень:", new HashSet<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, true));
-            WriteLine($"Без рекурси: {ExponentNotRecuresion(number, degree)}");
-            WriteLine($"С рекурсией: {ExponentNotRecuresion(number, degree)}");
+            WriteLine($"Без рекурси: { ExponentNotRecuresion(number, degree) }");
+            WriteLine($"С рекурсией: { ExponentRecuresion(number, degree) }");
+            WriteLine($"Рекурсивно, используя свойство чётности степени: { ExponentRecuresion2(number, degree) }");
         }
 
         private string ExponentNotRecuresion(int number, int degree)
@@ -100,14 +102,73 @@ namespace GB_Algoritmen_Lesson_2
             return number.ToString();
         }
 
-        private string ExponentRecuresion(int number, int degree) => Recuresion(number, number, degree);
+        private string ExponentRecuresion(int number, int degree) => Recuresion(number, ref number, degree).ToString();
         
-        private string Recuresion(int number1, int number2, int degree)
+        private int Recuresion(int number, ref int numberAnswer, int degree)
         {
-            number2 = number2 * number1;
+            numberAnswer *= number;
             degree--;
-            Recuresion(number1, number2, degree);
-            return number2.ToString();
+            if(degree > 1) Recuresion(number,ref numberAnswer, degree);
+            return numberAnswer;
+        }
+
+        private string ExponentRecuresion2(int number, int degree) => Recuresion2(number, ref number, degree).ToString();
+        private int Recuresion2(int number, ref int numberAnswer, int degree)
+        {
+            if (degree == 1) return numberAnswer;
+            if (degree % 2 != 0)
+            {
+                degree--;
+                numberAnswer = Recuresion2(number, ref numberAnswer, degree);
+                return numberAnswer *= number;
+            }
+            else
+            {
+                numberAnswer *= numberAnswer;
+                return Recuresion2(number, ref numberAnswer, degree /= 2);
+            }
+        }
+    }
+
+    class Calculator : Act
+    {
+        public override void Work()
+        {
+            WriteLine($"С использованием массива: { Massiv(20) }");
+            int t = 0;
+            WriteLine($"С использованием рекурсии: { Recuresion(20, ref t) }");
+        }
+
+        private string Massiv(int n)
+        {
+            var mass = new HashSet<int[]>();
+            for (int i = 0; i< n; ++i)
+            {
+                for (int j = 0; j < )
+            }
+            
+            return "";
+        }
+
+        Func<IEnumerable<int>, IEnumerable<IEnumerable<int>>> getAllSubsets = xs =>
+            (xs == null || !xs.Any()) ? Enumerable.Empty<IEnumerable<int>>()
+        :  xs.Skip(1).Any()
+        ? getAllSubsets(xs.Skip(1))
+                .SelectMany(ys => new [] { ys, xs.Take(1).Concat(ys) })
+            : new [] { Enumerable.Empty<int>(), xs.Take(1) };
+
+private string Recuresion(int n, ref int step)
+        {
+            if (n <= 0)
+            {
+                if (n == 0) step++;
+            }
+            else
+            {
+                Recuresion(n - 1, ref step);
+                Recuresion(n / 2, ref step);
+            }
+            return step.ToString();
         }
     }
 }
